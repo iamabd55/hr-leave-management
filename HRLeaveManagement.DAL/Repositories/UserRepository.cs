f65504cc7966c50c81efbe7e7709baf8ffbc7a9b
Abdul Rehman Ali <arehmanali000@gmail.com>
2026-07-02 21:22:23 +0500

    User repository to handle user's db operations

---
+++ HRLeaveManagement.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using HRLeaveManagement.DAL.Data;
+using HRLeaveManagement.DAL.Interfaces;
+using HRLeaveManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRLeaveManagement.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
