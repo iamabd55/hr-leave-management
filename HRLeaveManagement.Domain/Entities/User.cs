40b896823a69364ad26ee48748b695ecd15d7dfd
Abdul Rehman Ali <arehmanali000@gmail.com>
2026-07-02 21:19:29 +0500

    Made a new entity User for jwt auth

---
+++ HRLeaveManagement.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } // "Admin" or "Employee"
+
+
+        // Optional link — set when Role == "Employee", null for Admins
+        public int? EmployeeId { get; set; }
+        public Employee? Employee { get; set; }
+    }
+}
